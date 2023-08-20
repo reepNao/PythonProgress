b7e47cbf65f45cba2acf70deae55dd6bb57256fe
Recep Battal <recep.buss@gmail.com>
2023-08-20 13:52:29 +0300

    Update todo_app.py

--- python101 examples/todo_app.py
+++ python101 examples/todo_app.py
@@ -6,47 +6,47 @@ class ToDoList:
 
     def add_task(self, task):
         self.tasks.append(task)
-        print("Görev eklendi:", task)
+        print("Task Added:", task)
 
     def show_tasks(self):
         if self.tasks:
-            print("Yapılacaklar Listesi:")
+            print("To-Do List:")
             for idx, task in enumerate(self.tasks, start=1):
                 print("%d %s " % (idx, task))
         else:
-            print("Yapılacak görev yok.")
+            print("There is no task to be done.")
 
     def complete_task(self, task_index):
         if 1 <= task_index <= len(self.tasks):
             completed_task = self.tasks.pop(task_index - 1)
-            print("Tamamlanan görev:", completed_task)
+            print("Completed task:", completed_task)
         else:
-            print("Geçersiz görev numarası.")
+            print("Invalid task number.")
 
 def main():
     todo_list = ToDoList()
 
     while True:
-        print("\n1. Görev Ekle")
-        print("2. Görevleri Göster")
-        print("3. Görev Tamamlandı")
-        print("4. Çıkış\n")
-        choice = input("\nSeçiminizi yapın: ")
+        print("\n1. Add Task")
+        print("2. Show Tasks")
+        print("3. The job is done")
+        print("4. Exit\n")
+        choice = input("\nMake your selection: ")
 
         if choice == "1":
-            task = input("\nYeni görevi girin: ")
+            task = input("\nEnter the new task: ")
             todo_list.add_task(task)
         elif choice == "2":
             todo_list.show_tasks()
         elif choice == "3":
             todo_list.show_tasks()
-            task_index = int(input("\nTamamlanan görevin numarasını girin: "))
+            task_index = int(input("\nEnter the number of the completed task: "))
             todo_list.complete_task(task_index)
         elif choice == "4":
-            print("Çıkıyor...\n")
+            print("Exiting...\n")
             break
         else:
-            print("Geçersiz seçenek.")
+            print("Invalid option.")
 
 if __name__ == "__main__":
     main()
