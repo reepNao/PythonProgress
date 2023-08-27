3d454e967a203b12e9cd5c6c63b40755b93f84a5
Recep Battal <recep.buss@gmail.com>
2023-08-27 15:34:50 +0300

    DNF sort algoritm but easier

---
+++ python101 examples/DNF_problem_but_wTurkey.py
@@ -0,0 +1,43 @@
+import random
+
+# Python program to sort a sequence containing only red (0) and white (1) in a single pass.
+red = 0    # The first color of the flag.
+white = 1  # The second color of the flag.
+colors = (red, white, red)
+
+
+def turkish_flag_sort(sequence: list) -> list:
+    """
+    A pure Python implementation of Turkish Flag sort algorithm.
+    :param sequence: A sequence of 2 unique integer values (0, 1)
+    :return: The same collection in ascending order
+    """
+    if not sequence:
+        return []
+
+    low, mid, high = 0, 0, len(sequence) - 1
+
+    while mid <= high:
+        if sequence[mid] == colors[0]:
+            sequence[low], sequence[mid] = sequence[mid], sequence[low]
+            low += 1
+            mid += 1
+        elif sequence[mid] == colors[1]:
+            mid += 1
+        else:
+            raise ValueError(f"The elements inside the sequence must contain only {colors} values")
+
+    return sequence
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+
+    num_elements = int(input("Enter the number of elements: "))
+    unsorted = [random.choice([red, white]) for _ in range(num_elements)]
+    print(f"Unsorted sequence   : {unsorted}")
+    sorted_sequence = turkish_flag_sort(unsorted)
+    print(f"Sorted sequence     : {sorted_sequence}")
+
